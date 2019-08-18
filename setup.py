e95348d47c812ef4e30cc7cfefd657788b4b791e
Wouter Wijsman <wwijsman@live.nl>
2019-08-18 19:26:34 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+setup(
+    name="VaporOS-flatpak-manager",
+    version="1.0",
+    packages=find_packages(),
+    scripts=["vaporos-flatpak-manager"],
+
+    data_files = [('', ['LICENSE'])],
+
+    #metadata
+    author="Wouter wijsman",
+    author_email="wwijsman@live.nl",
+    description="A flatpak frontend for SteamOS",
+    keywords="flatpak steamos vaporos installer",
+    url="http://vaporos.net/",
+    project_urls={
+        "Bug Tracker": "https://github.com/sharkwouter/vaporos-flatpak-manager/issues",
+        "Documentation": "https://github.com/sharkwouter/vaporos-flatpak-manager/blob/master/README.md",
+        "Source Code": "https://github.com/sharkwouter/vaporos-flatpak-manager",
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ]
+)
