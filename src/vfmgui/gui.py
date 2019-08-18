c0d2390842efdebb38c915758c360fbf22634c17
Wouter Wijsman <wwijsman@live.nl>
2019-08-18 17:37:20 +0200

    Removed some unused color variables

--- src/vfmgui/gui.py
+++ src/vfmgui/gui.py
@@ -3,11 +3,6 @@ import vfmflatpak
 
 
 class Color():
-    RED = 255, 0, 0
-    BLUE = 0, 0, 255
-    GREEN = 0,255,0
-    BLACK = 0, 0, 0
-    WHITE = 255, 255 ,255
     BACKGROUND = 19, 139, 67
     BUTTON = 20, 167, 92
     BUTTON_SELECTED = 108, 196, 154
