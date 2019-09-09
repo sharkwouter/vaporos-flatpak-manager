4536cbeb3911b2fad26232d083ebf2be2bfff625
Wouter Wijsman <wwijsman@live.nl>
2019-09-09 19:42:08 +0200

    Added some information to the application menu screen

--- vfmgui/application_menu.py
+++ vfmgui/application_menu.py
@@ -1,10 +1,13 @@
 from vfmgui.menu import Menu
 import vfmgui
+import pygame
+
 
 class ApplicationMenuButtons:
     install = "Install"
     uninstall = "Uninstall"
     back = "back"
+
 
 class ApplicationMenu(Menu):
     def __init__(self, application):
@@ -14,9 +17,38 @@ class ApplicationMenu(Menu):
         self.buttons = []
         self.buttons.append(vfmgui.Button(ApplicationMenuButtons.install))
         self.buttons.append(vfmgui.Button(ApplicationMenuButtons.back))
+        self.font_title = pygame.font.Font(vfmgui.Fonts.REGULAR, 64)
+        self.font_small = pygame.font.Font(vfmgui.Fonts.REGULAR, 48)
+
+        try:
+            loaded_image = pygame.image.load(self.application.get_image())
+            self.image = pygame.transform.scale(loaded_image, (128, 128))
+        except:
+            self.image = None
 
     def draw(self, screen):
         screen_width, screen_height = screen.get_size()
+        item_height = (screen_height-160)/7
+
+        # Draw application name
+        text = self.font_title.render(str(self.application), True, vfmgui.Colors.TEXT_SUBTITLE)
+        rect_text = pygame.Rect(screen_width / 2 - text.get_width() / 2, 80 + item_height * 1 - text.get_height() / 2,
+                                text.get_width(), text.get_height())
+        screen.blit(text, rect_text)
+
+        # Draw application description
+        text = self.font_small.render(self.application.description, True, vfmgui.Colors.TEXT_SUBTITLE)
+        rect_text = pygame.Rect(screen_width / 2 - text.get_width() / 2,
+                                80 + item_height * 2 - text.get_height() / 2,
+                                text.get_width(), text.get_height())
+        screen.blit(text, rect_text)
+
+        # Draw image
+        if self.image is not None:
+            rect_image = pygame.Rect(screen_width / 2 - 64, 80+item_height*3, 128, 128)
+            screen.blit(self.image, rect_image)
+
+        # Draw buttons
         for index, button in enumerate(self.buttons):
             # Depending on the state of the application, change the install button to uninstall or the other way around
             if button.text == ApplicationMenuButtons.install and self.application.installed:
@@ -25,10 +57,9 @@ class ApplicationMenu(Menu):
                 button.text = ApplicationMenuButtons.install
             selected = (index == self.selected)
             button_width = screen_width/3
-            button_height = (screen_height-160)/7
             button_x = screen_width/2-button_width/2
-            button_y = 80+button_height*2+button_height*index
-            button.draw(button_x, button_y, button_width, button_height, selected, screen)
+            button_y = 80+item_height*5+item_height*index
+            button.draw(button_x, button_y, button_width, item_height, selected, screen)
 
     def get_selected_button(self):
         return self.buttons[self.selected].text
